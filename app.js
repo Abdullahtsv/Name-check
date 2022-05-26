0e415802faf1b90af6893b4912782b68e973cbcc
Abdullah Bhatti <99602533+Abdullahtsv@users.noreply.github.com>
2022-05-26 17:56:33 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var yourName = prompt("Enter your name", "Abdullah");
+var myName = "Abdullah";
+
+if(yourName === myName){
+    document.write("<h1>" + "Hello " + myName + "</h1>")
+}
+else{
+    document.write("<h1>" + "Your are " + yourName + " not " + myName + "</h1>")
+}
